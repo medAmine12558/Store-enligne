5a4490a1634f45fd0e0d2063b7a8f479848bedf4
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-09-27 17:46:08 +0100

    slide show + footer

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,8 +5,14 @@ import Add from './Pages/Admin/Categorie/Add.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path='/admin/cat' element={<Add />} />
+    <Route path='/user/home' element={<App />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
