f282ef12ac9ddfabb33d6a74cbc9a132ace7b1cd
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-09-27 15:41:31 +0100

    slide show

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Add from './Pages/Admin/Categorie/Add.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/cat' element={<Add />} />
-    </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
