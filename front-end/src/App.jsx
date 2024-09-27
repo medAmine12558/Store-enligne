b1f0b81b63726a59d78e545ac24000b63b79452e
zakaria elaroui <zakariaelaroui122@gmail.com>
2024-09-27 23:15:53 +0100

    change to slideShow

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Slideshow from './Pages/User/Components/HomePage/SlideShow'
 import Footer from './Pages/User/Components/HomePage/Footer'
--- front-end/src/Pages/User/Components/HomePage/Footer.jsx
+++ front-end/src/Pages/User/Components/HomePage/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Grid, Typography, Link, Box } from '@mui/material';
 import { Facebook, Instagram, Twitter } from '@mui/icons-material';
 
+
 const Footer = () => {
   return (
-    <Box sx={{ backgroundColor: '#f9f9f9', padding: '20px', marginTop: 'auto' }}>
+    <Box sx={{ backgroundColor: 'rgb(34, 41, 53)',color:"#cfd3d7", padding: '20px', marginTop: 'auto' }}>
       <Grid container spacing={4} justifyContent="center">
         {/* About Us Section */}
         <Grid item xs={12} md={4}>
@@ -21,7 +22,7 @@ const Footer = () => {
           <Typography variant="h6" gutterBottom>
             Contact Us
           </Typography>
-          <Typography variant="body2">casablanca, barnoussi Kodse</Typography>
+          <Typography variant="body2">Sidi Bernoussi, casablanca</Typography>
           <Typography variant="body2">Email: fozyrstore@gmail.com</Typography>
           <Typography variant="body2">Phone: +212 788993355</Typography>
         </Grid>
@@ -45,9 +46,8 @@ const Footer = () => {
         </Grid>
       </Grid>
 
-      {/* Copyright Section */}
-      <Box textAlign="center" marginTop="20px">
-        <Typography variant="body2" color="textSecondary">
+      <Box textAlign="center" marginTop="20px" >
+        <Typography variant="body2" color="textSecondary" sx={{color:"#cfd3d7"}}>
           Copyright © <Link href="#">Fozyr Store</Link> 2025.
         </Typography>
       </Box>
